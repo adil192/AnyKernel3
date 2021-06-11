626948236a0150bf161f04037f6216be4818fbd8
Adil Hanney <adilh+github@orchid.anonaddy.com>
2021-06-11 22:12:27 +0100

    add my kernel image, version 1.0

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=hyper kernel by steve
+kernel.string=adil kernel, based on hyper kernel by steve
 do.devicecheck=1
 do.modules=0
 do.systemless=1
