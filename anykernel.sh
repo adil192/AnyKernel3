8c6810e81fc20a8ce0354673c4a71034dc61298c
Adil Hanney <adilh+github@orchid.anonaddy.com>
2021-11-19 00:00:42 +0000

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,23 +4,21 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=Tab S7 Wi-Fi kernel by Adil
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
+device.name1=gts7lwifi
+device.name2=tabs7
+device.name3=SM-T870
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/platform/soc/1d84000.ufshc/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
@@ -77,4 +75,3 @@ write_boot;
 
 #flash_boot;
 ## end vendor_boot install
-
